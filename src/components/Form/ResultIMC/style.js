6f7cd1eaea60abc86a1093d028005e4c76f32581
anthony <anthonytguilherme@hotmail.com>
2023-07-18 20:46:51 -0300

    finish some styles

---
+++ src/components/Form/ResultIMC/style.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+
+
+const styles = StyleSheet.create({
+  resultIMC: {
+    fontSize: 48,
+    fontWeight: "bold",
+    width: "100%",
+    alignItems: "center",
+    flex: 1,
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    color: "#FF0043",
+  },
+  numberImc: {
+    fontSize: 48,
+    color: "#FF0043",
+    fontWeight: "bold",
+  },
+  information: {
+    fontSize: 18,
+    color: "#FF0043",
+    fontWeight: "bold",
+  }
+});
+
+export default styles;
